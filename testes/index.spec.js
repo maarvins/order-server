830daf576152805a51164bc789ad542aa2c34687
Marcus Sena <maarvins.19@gmail.com>
2021-08-12 18:23:30 -0300

    fix: change the expected value in my test

--- testes/index.spec.js
+++ testes/index.spec.js
@@ -2,6 +2,6 @@ const imported = require("../src/status")
 
 describe("Testing my function imported", () => {
   it("Return an object with a boolean value atribute", () => {
-    expect(imported.run()).toStrictEqual({running: expect.any(Boolean)})
+    expect(imported.run()).toStrictEqual({running: true})
   })
 })
